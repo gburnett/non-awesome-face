e2fcf2bda7ecfde5ba4368f2e081fe30259c32cb
gburnett <garyburnett3000@gmail.com>
2018-07-26 20:23:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,355 @@
+const chalk = require("chalk");
+module.exports = [
+  chalk.black(
+    "                                                                            "
+  ) + "\n",
+  chalk.black("                                ") +
+    chalk.bgYellow("       ") +
+    chalk.black("                                     ") +
+    "\n",
+  chalk.black("                         ") +
+    chalk.bgYellow("                     ") +
+    chalk.black("                              ") +
+    "\n",
+  chalk.black("                     ") +
+    chalk.bgYellow("                             ") +
+    chalk.black("                          ") +
+    "\n",
+  chalk.black("                  ") +
+    chalk.bgYellow("                                   ") +
+    chalk.black("                       ") +
+    "\n",
+  chalk.black("                ") +
+    chalk.bgYellow("                                       ") +
+    chalk.black("                     ") +
+    "\n",
+  chalk.black("              ") +
+    chalk.bgYellow("                                           ") +
+    chalk.black("                   ") +
+    "\n",
+  chalk.black("            ") +
+    chalk.bgYellow("                                               ") +
+    chalk.black("                 ") +
+    "\n",
+  chalk.black("          ") +
+    chalk.bgYellow("                                                   ") +
+    chalk.black("               ") +
+    "\n",
+  chalk.black("         ") +
+    chalk.bgYellow("                                                     ") +
+    chalk.black("              ") +
+    "\n",
+  chalk.black("        ") +
+    chalk.bgYellow("    ") +
+    chalk.black("     ") +
+    chalk.bgYellow("                           ") +
+    chalk.black("      ") +
+    chalk.bgYellow("             ") +
+    chalk.black("             ") +
+    "\n",
+  chalk.black("       ") +
+    chalk.bgYellow("  ") +
+    chalk.black("     ") +
+    chalk.bgWhite("  ") +
+    chalk.black("    ") +
+    chalk.bgYellow("                     ") +
+    chalk.black("     ") +
+    chalk.bgWhite("  ") +
+    chalk.black("     ") +
+    chalk.bgYellow("           ") +
+    chalk.black("            ") +
+    "\n",
+  chalk.black("      ") +
+    chalk.bgYellow("  ") +
+    chalk.black("   ") +
+    chalk.bgWhite("        ") +
+    chalk.black("   ") +
+    chalk.bgYellow("                 ") +
+    chalk.black("    ") +
+    chalk.bgWhite("         ") +
+    chalk.black("   ") +
+    chalk.bgYellow("          ") +
+    chalk.black("           ") +
+    "\n",
+  chalk.black("     ") +
+    chalk.bgYellow("  ") +
+    chalk.black("   ") +
+    chalk.bgWhite("   ") +
+    chalk.black("      ") +
+    chalk.bgWhite(" ") +
+    chalk.black("   ") +
+    chalk.bgYellow("               ") +
+    chalk.black("   ") +
+    chalk.bgWhite("   ") +
+    chalk.black("        ") +
+    chalk.bgWhite(" ") +
+    chalk.black("   ") +
+    chalk.bgYellow("          ") +
+    chalk.black("          ") +
+    "\n",
+  chalk.black("    ") +
+    chalk.bgYellow("  ") +
+    chalk.black("   ") +
+    chalk.bgWhite("   ") +
+    chalk.black("            ") +
+    chalk.bgYellow("             ") +
+    chalk.black("   ") +
+    chalk.bgWhite("   ") +
+    chalk.black("          ") +
+    chalk.bgWhite("  ") +
+    chalk.black("  ") +
+    chalk.bgYellow("          ") +
+    chalk.black("         ") +
+    "\n",
+  chalk.black("    ") +
+    chalk.bgYellow("  ") +
+    chalk.black("  ") +
+    chalk.bgWhite("   ") +
+    chalk.black("          ") +
+    chalk.bgWhite(" ") +
+    chalk.black("  ") +
+    chalk.bgYellow("             ") +
+    chalk.black("  ") +
+    chalk.bgWhite("    ") +
+    chalk.black("           ") +
+    chalk.bgWhite(" ") +
+    chalk.black("   ") +
+    chalk.bgYellow("         ") +
+    chalk.black("         ") +
+    "\n",
+  chalk.black("   ") +
+    chalk.bgYellow("   ") +
+    chalk.black("  ") +
+    chalk.bgWhite("   ") +
+    chalk.black("          ") +
+    chalk.bgWhite("  ") +
+    chalk.black("  ") +
+    chalk.bgYellow("           ") +
+    chalk.black("  ") +
+    chalk.bgWhite("     ") +
+    chalk.black("           ") +
+    chalk.bgWhite("  ") +
+    chalk.black("  ") +
+    chalk.bgYellow("          ") +
+    chalk.black("        ") +
+    "\n",
+  chalk.black("   ") +
+    chalk.bgYellow("  ") +
+    chalk.black("   ") +
+    chalk.bgWhite("   ") +
+    chalk.black("          ") +
+    chalk.bgWhite("  ") +
+    chalk.black("  ") +
+    chalk.bgYellow("           ") +
+    chalk.black("  ") +
+    chalk.bgWhite("     ") +
+    chalk.black("           ") +
+    chalk.bgWhite("  ") +
+    chalk.black("  ") +
+    chalk.bgYellow("          ") +
+    chalk.black("        ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow("   ") +
+    chalk.black("   ") +
+    chalk.bgWhite("    ") +
+    chalk.black("        ") +
+    chalk.bgWhite("   ") +
+    chalk.black("  ") +
+    chalk.bgYellow("           ") +
+    chalk.black("  ") +
+    chalk.bgWhite("      ") +
+    chalk.black("         ") +
+    chalk.bgWhite("   ") +
+    chalk.black("  ") +
+    chalk.bgYellow("           ") +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow("    ") +
+    chalk.black("  ") +
+    chalk.bgCyan("      ") +
+    chalk.black("   ") +
+    chalk.bgCyan("      ") +
+    chalk.black("  ") +
+    chalk.bgYellow("           ") +
+    chalk.black("  ") +
+    chalk.bgCyan("         ") +
+    chalk.black("   ") +
+    chalk.bgCyan("      ") +
+    chalk.black("  ") +
+    chalk.bgYellow("           ") +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow("    ") +
+    chalk.black("  ") +
+    chalk.bgCyan("              ") +
+    chalk.black("  ") +
+    chalk.bgYellow("            ") +
+    chalk.black("  ") +
+    chalk.bgCyan("                  ") +
+    chalk.black("  ") +
+    chalk.bgYellow("           ") +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow("     ") +
+    chalk.black("                 ") +
+    chalk.bgYellow("             ") +
+    chalk.black("           ") +
+    chalk.bgCyan("    ") +
+    chalk.black("     ") +
+    chalk.bgYellow("            ") +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow("                                              ") +
+    chalk.bgCyan("    ") +
+    chalk.bgYellow("                 ") +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow("                                               ") +
+    chalk.bgCyan("   ") +
+    chalk.bgYellow("                 ") +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow(
+      "                                                                   "
+    ) +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow("                               ") +
+    chalk.black("   ") +
+    chalk.bgYellow("                                 ") +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow("                        ") +
+    chalk.black("       ") +
+    chalk.bgMagenta("   ") +
+    chalk.black("     ") +
+    chalk.bgYellow("                            ") +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("  ") +
+    chalk.bgYellow("                     ") +
+    chalk.black("   ") +
+    chalk.bgMagenta("               ") +
+    chalk.black("   ") +
+    chalk.bgYellow("                         ") +
+    chalk.black("       ") +
+    "\n",
+  chalk.black("   ") +
+    chalk.bgYellow("                  ") +
+    chalk.black("  ") +
+    chalk.bgMagenta("                     ") +
+    chalk.black("  ") +
+    chalk.bgYellow("                      ") +
+    chalk.black("        ") +
+    "\n",
+  chalk.black("   ") +
+    chalk.bgYellow("                ") +
+    chalk.black("  ") +
+    chalk.bgMagenta("                         ") +
+    chalk.black("  ") +
+    chalk.bgYellow("                    ") +
+    chalk.black("        ") +
+    "\n",
+  chalk.black("    ") +
+    chalk.bgYellow("              ") +
+    chalk.black("  ") +
+    chalk.bgMagenta("                           ") +
+    chalk.black("  ") +
+    chalk.bgYellow("                  ") +
+    chalk.black("         ") +
+    "\n",
+  chalk.black("    ") +
+    chalk.bgYellow("             ") +
+    chalk.black("  ") +
+    chalk.bgMagenta("                             ") +
+    chalk.black("  ") +
+    chalk.bgYellow("                 ") +
+    chalk.black("         ") +
+    "\n",
+  chalk.black("     ") +
+    chalk.bgYellow("           ") +
+    chalk.black("  ") +
+    chalk.bgMagenta("                         +m????") +
+    chalk.black("  ") +
+    chalk.bgYellow("               ") +
+    chalk.black("          ") +
+    "\n",
+  chalk.black("      ") +
+    chalk.bgYellow("         ") +
+    chalk.black("  ") +
+    chalk.bgMagenta("                       +?????????") +
+    chalk.black("  ") +
+    chalk.bgYellow("             ") +
+    chalk.black("           ") +
+    "\n",
+  chalk.black("       ") +
+    chalk.bgYellow("        ") +
+    chalk.black("  ") +
+    chalk.bgMagenta("                     +????????????") +
+    chalk.black("  ") +
+    chalk.bgYellow("           ") +
+    chalk.black("            ") +
+    "\n",
+  chalk.black("        ") +
+    chalk.bgYellow("       ") +
+    chalk.black("  ") +
+    chalk.bgMagenta("                    +??????????????") +
+    chalk.black(" ") +
+    chalk.bgYellow("          ") +
+    chalk.black("             ") +
+    "\n",
+  chalk.black("         ") +
+    chalk.bgYellow("      ") +
+    chalk.black("  ") +
+    chalk.bgMagenta("                   +???????????????") +
+    chalk.black("  ") +
+    chalk.bgYellow("        ") +
+    chalk.black("              ") +
+    "\n",
+  chalk.black("           ") +
+    chalk.bgYellow("     ") +
+    chalk.black(". ") +
+    chalk.bgMagenta("                  ????????????????") +
+    chalk.black("  ") +
+    chalk.bgYellow("      ") +
+    chalk.black("                ") +
+    "\n",
+  chalk.black("            ") +
+    chalk.bgYellow("    ") +
+    chalk.black("                                        ") +
+    chalk.bgYellow("   ") +
+    chalk.black("                 ") +
+    "\n",
+  chalk.black("              ") +
+    chalk.bgYellow("                                           ") +
+    chalk.black("                   ") +
+    "\n",
+  chalk.black("                ") +
+    chalk.bgYellow("                                       ") +
+    chalk.black("                     ") +
+    "\n",
+  chalk.black("                  ") +
+    chalk.bgYellow("                                   ") +
+    chalk.black("                       ") +
+    "\n",
+  chalk.black("                     ") +
+    chalk.bgYellow("                             ") +
+    chalk.black("                          ") +
+    "\n",
+  chalk.black("                         ") +
+    chalk.bgYellow("                     ") +
+    chalk.black("                              ") +
+    "\n",
+  chalk.black(
+    "                                                                            "
+  ) + "\n"
+].join("");
